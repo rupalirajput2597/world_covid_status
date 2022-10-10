fb19f91b162229a000afefb1e763a1dbc78e889a
rupalirajput2597 <rupalirajput2597@gmail.com>
2022-10-10 19:59:06 +0530

    moved main.dart into lib folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../core/core.dart';
-import '../home/home.dart';
-import '../navigator/navigator.dart';
+import 'core/core.dart';
+import 'home/home.dart';
+import 'navigator/navigator.dart';
 
 void main() {
   runApp(const WorldCovidStatusApp());
